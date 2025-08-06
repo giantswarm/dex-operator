35fe0f721721bbdd36396364c71214bbc5cc63fd
Spyros Synodinos <spyros@giantswarm.io>
2025-08-06 14:50:40 +0300

    Restart Pods on Secrets renewal

--- pkg/idp/self_renewal_test.go
+++ pkg/idp/self_renewal_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"gopkg.in/yaml.v2"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -15,7 +16,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	"github.com/giantswarm/apiextensions-application/api/v1alpha1"
-
 	"github.com/giantswarm/dex-operator/pkg/dex"
 	"github.com/giantswarm/dex-operator/pkg/idp/provider"
 )
@@ -457,6 +457,443 @@ func TestAddSelfRenewalAnnotation(t *testing.T) {
 			timestamp := tc.secret.Annotations[SelfRenewalAnnotation]
 			if _, err := time.Parse(time.RFC3339, timestamp); err != nil {
 				t.Errorf("Invalid timestamp format in annotation: %s", timestamp)
+			}
+		})
+	}
+}
+func TestRestartDeployment(t *testing.T) {
+	testCases := []struct {
+		name               string
+		deploymentName     string
+		namespace          string
+		existingDeployment *appsv1.Deployment
+		expectError        bool
+		expectAnnotation   bool
+	}{
+		{
+			name:           "Successfully restart deployment",
+			deploymentName: "dex-app",
+			namespace:      "giantswarm",
+			existingDeployment: &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "dex-app",
+					Namespace: "giantswarm",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{},
+					},
+				},
+			},
+			expectAnnotation: true,
+		},
+		{
+			name:           "Deployment with existing annotations",
+			deploymentName: "dex-operator",
+			namespace:      "giantswarm",
+			existingDeployment: &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "dex-operator",
+					Namespace: "giantswarm",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Annotations: map[string]string{
+								"existing-annotation": "value",
+							},
+						},
+					},
+				},
+			},
+			expectAnnotation: true,
+		},
+		{
+			name:           "Deployment not found",
+			deploymentName: "non-existent",
+			namespace:      "giantswarm",
+			expectError:    true,
+		},
+		{
+			name:           "Deployment already restarted with same timestamp",
+			deploymentName: "dex-app",
+			namespace:      "giantswarm",
+			existingDeployment: &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "dex-app",
+					Namespace: "giantswarm",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Annotations: map[string]string{
+								RestartAnnotation: "2024-01-01T12:00:00Z",
+							},
+						},
+					},
+				},
+			},
+			expectAnnotation: false, // Same timestamp, no update
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			scheme := runtime.NewScheme()
+			appsv1.AddToScheme(scheme)
+			corev1.AddToScheme(scheme)
+
+			fakeClientBuilder := fake.NewClientBuilder().WithScheme(scheme)
+			if tc.existingDeployment != nil {
+				fakeClientBuilder = fakeClientBuilder.WithObjects(tc.existingDeployment)
+			}
+			fakeClient := fakeClientBuilder.Build()
+
+			service := Service{
+				Client: fakeClient,
+				log:    ctrl.Log.WithName("test"),
+				app:    getTestApp(),
+			}
+
+			timestamp := "2024-01-01T12:00:00Z"
+			if tc.name == "Deployment already restarted with same timestamp" {
+				// Use the same timestamp that's already set
+				timestamp = "2024-01-01T12:00:00Z"
+			}
+
+			err := service.restartDeployment(ctx, tc.deploymentName, tc.namespace, timestamp)
+
+			// Check error expectation
+			if tc.expectError && err == nil {
+				t.Errorf("Expected error but got none")
+			}
+			if !tc.expectError && err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+
+			// If we expect the annotation to be set, verify it
+			if tc.expectAnnotation && !tc.expectError {
+				updatedDeployment := &appsv1.Deployment{}
+				err := fakeClient.Get(ctx, types.NamespacedName{
+					Name:      tc.deploymentName,
+					Namespace: tc.namespace,
+				}, updatedDeployment)
+				if err != nil {
+					t.Errorf("Failed to get updated deployment: %v", err)
+					return
+				}
+
+				if updatedDeployment.Spec.Template.Annotations == nil {
+					t.Errorf("Expected annotations to be set")
+					return
+				}
+
+				if updatedDeployment.Spec.Template.Annotations[RestartAnnotation] != timestamp {
+					t.Errorf("Expected restart annotation to be %s, got %s",
+						timestamp, updatedDeployment.Spec.Template.Annotations[RestartAnnotation])
+				}
+
+				// Check that existing annotations are preserved
+				if tc.name == "Deployment with existing annotations" {
+					if updatedDeployment.Spec.Template.Annotations["existing-annotation"] != "value" {
+						t.Errorf("Expected existing annotations to be preserved")
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestRestartRelatedPods(t *testing.T) {
+	testCases := []struct {
+		name                string
+		existingDeployments []*appsv1.Deployment
+		appNamespace        string
+		expectedRestarts    []string // deployment names that should be restarted
+	}{
+		{
+			name:         "Restart dex-app and dex-operator in same namespace",
+			appNamespace: "giantswarm",
+			existingDeployments: []*appsv1.Deployment{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-app",
+						Namespace: "giantswarm",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-operator",
+						Namespace: "giantswarm",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+			},
+			expectedRestarts: []string{"dex-app", "dex-operator"},
+		},
+		{
+			name:         "Restart in different namespaces",
+			appNamespace: "org-example",
+			existingDeployments: []*appsv1.Deployment{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-app",
+						Namespace: "org-example",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-operator",
+						Namespace: "org-example",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-app",
+						Namespace: "giantswarm",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-operator",
+						Namespace: "giantswarm",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+			},
+			expectedRestarts: []string{"dex-app", "dex-operator"}, // In both namespaces
+		},
+		{
+			name:                "No deployments exist",
+			appNamespace:        "giantswarm",
+			existingDeployments: []*appsv1.Deployment{},
+			expectedRestarts:    []string{},
+		},
+		{
+			name:         "Only dex-app exists",
+			appNamespace: "giantswarm",
+			existingDeployments: []*appsv1.Deployment{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-app",
+						Namespace: "giantswarm",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+			},
+			expectedRestarts: []string{"dex-app"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			scheme := runtime.NewScheme()
+			appsv1.AddToScheme(scheme)
+			corev1.AddToScheme(scheme)
+
+			fakeClientBuilder := fake.NewClientBuilder().WithScheme(scheme)
+			for _, dep := range tc.existingDeployments {
+				fakeClientBuilder = fakeClientBuilder.WithObjects(dep)
+			}
+			fakeClient := fakeClientBuilder.Build()
+
+			service := Service{
+				Client: fakeClient,
+				log:    ctrl.Log.WithName("test"),
+				app: &v1alpha1.App{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-app",
+						Namespace: tc.appNamespace,
+					},
+				},
+			}
+
+			err := service.restartRelatedPods(ctx)
+			// We don't expect errors as the function continues even if some restarts fail
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+
+			// Verify that expected deployments have the restart annotation
+			for _, depName := range tc.expectedRestarts {
+				// Check in app namespace
+				deployment := &appsv1.Deployment{}
+				err := fakeClient.Get(ctx, types.NamespacedName{
+					Name:      depName,
+					Namespace: tc.appNamespace,
+				}, deployment)
+
+				if err == nil {
+					if deployment.Spec.Template.Annotations == nil ||
+						deployment.Spec.Template.Annotations[RestartAnnotation] == "" {
+						t.Errorf("Expected deployment %s/%s to have restart annotation",
+							tc.appNamespace, depName)
+					}
+				}
+
+				// Also check in giantswarm namespace if different
+				if tc.appNamespace != "giantswarm" {
+					deployment = &appsv1.Deployment{}
+					err = fakeClient.Get(ctx, types.NamespacedName{
+						Name:      depName,
+						Namespace: "giantswarm",
+					}, deployment)
+
+					if err == nil {
+						if deployment.Spec.Template.Annotations == nil ||
+							deployment.Spec.Template.Annotations[RestartAnnotation] == "" {
+							t.Errorf("Expected deployment giantswarm/%s to have restart annotation", depName)
+						}
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestCheckAndRotateServiceCredentialsWithRestart(t *testing.T) {
+	testCases := []struct {
+		name                string
+		providers           []provider.Provider
+		existingSecret      *corev1.Secret
+		existingDeployments []*appsv1.Deployment
+		expectedRotation    bool
+		expectedRestart     bool
+	}{
+		{
+			name: "Successful rotation triggers restart",
+			providers: []provider.Provider{
+				&testSelfRenewalProvider{
+					name:              "test-provider",
+					providerName:      "test",
+					owner:             "giantswarm",
+					supportsRenewal:   true,
+					shouldRotate:      true,
+					rotateCredentials: map[string]string{"client-id": "new-client", "client-secret": "new-secret"},
+				},
+			},
+			existingSecret: getTestCredentialsSecret(),
+			existingDeployments: []*appsv1.Deployment{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-app",
+						Namespace: "example",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "dex-operator",
+						Namespace: "example",
+					},
+					Spec: appsv1.DeploymentSpec{
+						Template: corev1.PodTemplateSpec{},
+					},
+				},
+			},
+			expectedRotation: true,
+			expectedRestart:  true,
+		},
+		{
+			name: "No rotation means no restart",
+			providers: []provider.Provider{
+				&testSelfRenewalProvider{
+					name:            "test-provider",
+					providerName:    "test",
+					owner:           "giantswarm",
+					supportsRenewal: true,
+					shouldRotate:    false,
+				},
+			},
+			existingSecret:      getTestCredentialsSecret(),
+			existingDeployments: []*appsv1.Deployment{},
+			expectedRotation:    false,
+			expectedRestart:     false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			scheme := runtime.NewScheme()
+			corev1.AddToScheme(scheme)
+			appsv1.AddToScheme(scheme)
+
+			fakeClientBuilder := fake.NewClientBuilder().WithScheme(scheme)
+			if tc.existingSecret != nil {
+				fakeClientBuilder = fakeClientBuilder.WithObjects(tc.existingSecret)
+			}
+			for _, dep := range tc.existingDeployments {
+				fakeClientBuilder = fakeClientBuilder.WithObjects(dep)
+			}
+			fakeClient := fakeClientBuilder.Build()
+
+			service := Service{
+				Client:                fakeClient,
+				log:                   ctrl.Log.WithName("test"),
+				app:                   getTestApp(),
+				providers:             tc.providers,
+				managementClusterName: "test-cluster",
+			}
+
+			err := service.CheckAndRotateServiceCredentials(ctx)
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+
+			// Check if deployments were restarted
+			if tc.expectedRestart {
+				for _, dep := range tc.existingDeployments {
+					deployment := &appsv1.Deployment{}
+					err := fakeClient.Get(ctx, types.NamespacedName{
+						Name:      dep.Name,
+						Namespace: dep.Namespace,
+					}, deployment)
+					if err != nil {
+						t.Errorf("Failed to get deployment: %v", err)
+						continue
+					}
+
+					if deployment.Spec.Template.Annotations == nil ||
+						deployment.Spec.Template.Annotations[RestartAnnotation] == "" {
+						t.Errorf("Expected deployment %s/%s to have restart annotation",
+							dep.Namespace, dep.Name)
+					} else {
+						// Verify the timestamp is recent (within last minute)
+						timestamp := deployment.Spec.Template.Annotations[RestartAnnotation]
+						parsedTime, err := time.Parse(time.RFC3339, timestamp)
+						if err != nil {
+							t.Errorf("Invalid timestamp format: %s", timestamp)
+						} else if time.Since(parsedTime) > time.Minute {
+							t.Errorf("Restart timestamp is too old: %s", timestamp)
+						}
+					}
+				}
 			}
 		})
 	}
