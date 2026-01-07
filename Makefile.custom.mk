7eef3d5c625848492c8af772e19554a552851b9d
Spyros Synodinos <spyros@giantswarm.io>
2026-01-07 17:49:55 +0200

    Revert makefile

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -146,10 +146,7 @@ controller-gen: $(CONTROLLER_GEN) ## Download controller-gen locally if necessar
 $(CONTROLLER_GEN): $(LOCALBIN)
 	test -s $(LOCALBIN)/controller-gen || GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-tools/cmd/controller-gen@$(CONTROLLER_TOOLS_VERSION)
 
-## setup-envtest version - pinned to avoid Go 1.25 coverage tooling issues
-ENVTEST_VERSION ?= v0.0.0-20240320141353-395cfc7486e6
-
 .PHONY: envtest
 envtest: $(ENVTEST) ## Download envtest-setup locally if necessary.
 $(ENVTEST): $(LOCALBIN)
-	test -s $(LOCALBIN)/setup-envtest || GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-runtime/tools/setup-envtest@$(ENVTEST_VERSION)
+	test -s $(LOCALBIN)/setup-envtest || GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
